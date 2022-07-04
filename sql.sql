9a8be10e88c5ac3d3892cf21113fc82049b039be
InshadS <inshad@hamon.in>
2022-07-04 15:17:07 +0530

    Updated sql file

--- sql.sql
+++ sql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE task(
-    task_id SERIAL PRIMARY KEY,
-    task_name VARCHAR(100),
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
     start_time TIMESTAMP,
     end_time TIMESTAMP,
     task_duration VARCHAR(50)
